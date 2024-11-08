2f0c8b0b5221f8cf0186bdcd2c38fcf5431bf383
Amanda Soares <amanda.soares@ifood.com.br>
2024-11-08 16:49:33 -0300

    chave api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Google maps
+    implementation(libs.play.services.maps)
+
 }
